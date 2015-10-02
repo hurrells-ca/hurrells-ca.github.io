49241beab1c1920eff34e4c277951e57833efbee
Stephen Hurrell <hurrells@hurrells.ca>
2015-10-02 11:09:32 -0400

    Quick Publish

--- script/publish.sh
+++ script/publish.sh
@@ -5,4 +5,4 @@ git add --all
 git commit -m "Quick Publish"
 git push origin master
 # Clone
-git checkout gh-pages && git rebase master && git push origin gh-pages && git checkout master
+# git checkout gh-pages && git rebase master && git push origin gh-pages && git checkout master
